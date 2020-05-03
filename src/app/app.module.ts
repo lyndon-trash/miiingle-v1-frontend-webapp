5bcde6c4c0d78120eb57b4b075231eb137235251
Lyndon Bibera <lyndon.bibera@headhuntr.io>
2020-05-03 19:12:46 +0800

    Install PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from './material/material.module';
 import { MainNavigationComponent } from './main-navigation/main-navigation.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { AuthenticationComponent } from './authentication/authentication.compone
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
